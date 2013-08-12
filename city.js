14dcff865a924ae5399096476fb56a37ba209fed
David Fisher <tibbon+github@gmail.com>
2013-08-12 15:24:36 -0400

    Single Var pattern accomplished

--- city.js
+++ city.js
@@ -1,11 +1,21 @@
-var scene, camera, renderer;
-var light, controls;
-var lastTime;
+var scene, camera, renderer, light, controls, lastTime;
 
 init();
 animate();
 
 function init() {
+  var plane,
+      geometry,
+      building,
+      city,
+      light,
+      shadow,
+      value,
+      color,
+      top,
+      bottom,
+      texture,
+      info;
 
   renderer = new THREE.WebGLRenderer( { antialias: false, alpha: false } );
 	renderer.setClearColor( 0xd8e7ff );
@@ -27,11 +37,11 @@ function init() {
 	light.position.set( 0.75, 1, 0.25 );
 	scene.add( light );
 
-	var plane = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshBasicMaterial( { color: 0x101018 } ) );
+	plane = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshBasicMaterial( { color: 0x101018 } ) );
 	plane.rotation.x = - 90 * Math.PI / 180;
 	scene.add( plane );
 
-	var geometry = new THREE.CubeGeometry( 1, 1, 1 );
+	geometry = new THREE.CubeGeometry( 1, 1, 1 );
 	geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
 	geometry.faces.splice( 3, 1 );
 	geometry.faceVertexUvs[0].splice( 3, 1 );
@@ -40,19 +50,19 @@ function init() {
 	geometry.faceVertexUvs[0][2][2].set( 0, 0 );
 	geometry.faceVertexUvs[0][2][3].set( 0, 0 );
 
-	var building = new THREE.Mesh( geometry );
-	var city = new THREE.Geometry();
+	building = new THREE.Mesh( geometry );
+	city = new THREE.Geometry();
 
-	var light = new THREE.Color( 0xffffff );
-	var shadow = new THREE.Color( 0x303050 );
+	light = new THREE.Color( 0xffffff );
+	shadow = new THREE.Color( 0x303050 );
 
 	for ( var i = 0; i < 20000; i ++ ) {
 
-		var value = 1 - Math.random() * Math.random();
-		var color = new THREE.Color().setRGB( value + Math.random() * 0.1, value, value + Math.random() * 0.1 );
+		value = 1 - Math.random() * Math.random();
+		color = new THREE.Color().setRGB( value + Math.random() * 0.1, value, value + Math.random() * 0.1 );
 
-		var top = color.clone().multiply( light );
-		var bottom = color.clone().multiply( shadow );
+		top = color.clone().multiply( light );
+		bottom = color.clone().multiply( shadow );
 
 		building.position.x = Math.floor( Math.random() * 200 - 100 ) * 10;
 		building.position.z = Math.floor( Math.random() * 200 - 100 ) * 10;
@@ -60,27 +70,20 @@ function init() {
 		building.scale.x = building.scale.z = Math.random() * Math.random() * Math.random() * Math.random() * 50 + 10;
 		building.scale.y = ( Math.random() * Math.random() * Math.random() * building.scale.x ) * 8 + 8;
 
-		var geometry = building.geometry;
+		geometry = building.geometry;
 
 		for ( var j = 0, jl = geometry.faces.length; j < jl; j ++ ) {
-
 			if ( j === 2 ) {
-
 				geometry.faces[ j ].vertexColors = [ color, color, color, color ];
-
 			} else {
-
 				geometry.faces[ j ].vertexColors = [ top, bottom, bottom, top ];
-
 			}
-
 		}
 
 		THREE.GeometryUtils.merge( city, building );
-
 	}
 
-	var texture = new THREE.Texture( generateTexture() );
+	texture = new THREE.Texture( generateTexture() );
 	texture.anisotropy = renderer.getMaxAnisotropy();
 	texture.needsUpdate = true;
 
@@ -89,7 +92,7 @@ function init() {
 
 	//
 
-	var info = document.createElement( 'div' );
+	info = document.createElement( 'div' );
 	info.style.position = 'absolute';
 	info.style.left = '0';
 	info.style.top = '15px';
@@ -104,12 +107,17 @@ function init() {
 }
 
 function generateTexture() {
+  var canvas,
+      context,
+      canvas2,
+      context;
 
-	var canvas = document.createElement( 'canvas' );
+
+	canvas = document.createElement( 'canvas' );
 	canvas.width = 32;
 	canvas.height = 64;
 
-	var context = canvas.getContext( '2d' );
+	context = canvas.getContext( '2d' );
 	context.fillStyle = '#ffffff';
 	context.fillRect( 0, 0, 32, 64 );
 
@@ -125,29 +133,24 @@ function generateTexture() {
 
 	}
 
-	var canvas2 = document.createElement( 'canvas' );
+	canvas2 = document.createElement( 'canvas' );
 	canvas2.width = 512;
 	canvas2.height = 1024;
 
-	var context = canvas2.getContext( '2d' );
+	context = canvas2.getContext( '2d' );
 	context.imageSmoothingEnabled = false;
 	context.webkitImageSmoothingEnabled = false;
 	context.mozImageSmoothingEnabled = false;
 	context.drawImage( canvas, 0, 0, canvas2.width, canvas2.height );
 
 	return canvas2;
-
 }
 
 function animate() {
-
+  var time;
 	requestAnimationFrame( animate );
-
-	var time = performance.now() / 1000;
-
+	time = performance.now() / 1000;
 	controls.update( time - lastTime );
 	renderer.render( scene, camera );
-
 	lastTime = time;
-
 }
